beddf97b936e29edcebba9cf7676206ef63556f6
satyam <satyamkale271@gmail.com>
2025-01-31 20:13:02 +0530

    objects and its interfaces

--- index.ts
+++ index.ts
@@ -6,10 +6,32 @@
 // let x: any = "pedro";
 // let xArr: any[] = ["pedro", 0, true];
 
-const concatenateStrings = (a: string, b: string): string => {
-  // string parameter and string return type
-  return a + b;
+// const concatenateStrings = (a: string, b: string): string => {
+//   // string parameter and string return type
+//   return a + b;
+// };
+
+// console.log(concatenateStrings("Hello", "world"));
+// console.log(concatenateStrings("5", "10"));
+
+interface UserInterface {
+  // it is BluePrint of object and how it looks like
+  id: number;
+  name: string;
+  age?: number; // ? means it is optional
+  greet(message: string): void; // function which is returning nothing
+}
+
+const User: UserInterface = {
+  id: 2,
+  name: "Satyam",
+  greet(message) {
+    console.log(message);
+  },
 };
 
-console.log(concatenateStrings("Hello", "world"));
-console.log(concatenateStrings("5", "10"));
+if (!User.age) {
+  console.log("User is not defined");
+} else {
+  console.log(User.age);
+}
