fd5b2048bb1b37155138565ba7caf5392d288365
satyam <satyamkale271@gmail.com>
2025-01-31 19:18:22 +0530

    return type and parameter type in ts

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 // some types in typescript
-let id: number = 5;
-let company: string = "Acme Corp";
-let isPublisher: boolean = true;
-let ids: number[] = [1, 2, 3, 4];
-let x: any = "pedro";
-let xArr: any[] = ["pedro", 0, true];
+// let id: number = 5;
+// let company: string = "Acme Corp";
+// let isPublisher: boolean = true;
+// let ids: number[] = [1, 2, 3, 4];
+// let x: any = "pedro";
+// let xArr: any[] = ["pedro", 0, true];
+
+const concatenateStrings = (a: string, b: string): string => {
+  // string parameter and string return type
+  return a + b;
+};
+
+console.log(concatenateStrings("Hello", "world"));
+console.log(concatenateStrings("5", "10"));
