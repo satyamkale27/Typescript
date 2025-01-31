5ef4cf5541e5344469c6bf182469a39c82276ed1
satyam <satyamkale271@gmail.com>
2025-01-31 18:53:19 +0530

    types in typescript

---
+++ index.ts
@@ -0,0 +1,7 @@
+// some types in typescript
+let id: number = 5;
+let company: string = "Acme Corp";
+let isPublisher: boolean = true;
+let ids: number[] = [1, 2, 3, 4];
+let x: any = "pedro";
+let xArr: any[] = ["pedro", 0, true];
