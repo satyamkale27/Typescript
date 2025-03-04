c04de74484dfeecb6846a9a388046c60ffb12db8
satyam <satyamkale271@gmail.com>
2025-03-04 16:13:44 +0530

    class, object, function typeNotation

--- morets/one/typeNotation.ts
+++ morets/one/typeNotation.ts
@@ -11,3 +11,16 @@ let now: Date = new Date();
 let colors: string[] = ["red", "green", "blue"];
 let myNumbers: number[] = [1, 2, 3];
 let truths: boolean[] = [true, true, true];
+
+// classes
+
+class Car {}
+
+let car: Car = new Car();
+
+// object literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
