d77123521119b8f7117f2832a2e8eaad8796bae8
satyam <satyamkale271@gmail.com>
2025-03-04 18:54:35 +0530

    interface

--- morets/one/typeNotation.ts
+++ morets/one/typeNotation.ts
@@ -24,3 +24,34 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
+
+// Function
+
+const logNumber: (i: number) => void = (i: number) => {
+  // notation   (i: number)=> void
+  console.log(i);
+};
+
+// when to use anotations
+// 1) Function that returns the "any" type
+
+// note:- variable of type any is bad
+
+const json = '{"x":10, "y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+/*
+note:-
+type interface is ability of ts that identifies the type of vatiable automatically
+for that the declariation and initiliazation of variable should done on same line 
+*/
+
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
