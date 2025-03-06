20e4d118e3a03c14f29d93f0fa01d2574ef2b483
satyam <satyamkale271@gmail.com>
2025-03-06 20:04:02 +0530

    interfaces

---
+++ morets/interfaces/interfaces.ts
@@ -0,0 +1,37 @@
+interface vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+const oldCivic = {
+  name: "civic",
+  year: 200,
+  broken: true,
+};
+
+const printVechicle = (vehicle: vehicle): void => {
+  console.log(`Name:${vehicle.name}`);
+  console.log(`Name:${vehicle.year}`);
+  console.log(`Name:${vehicle.broken}`);
+};
+
+printVechicle(oldCivic);
+
+/*
+
+// done using Anotation 
+
+const printVechicle = (vehicle: {
+  name: string;     // using annotations object structure has to be written in every function that makes code duplication, and hard to read
+  year: number;
+  broken: boolean;
+}): void => {
+  console.log(`Name:${vehicle.name}`);
+  console.log(`Name:${vehicle.year}`);
+  console.log(`Name:${vehicle.broken}`);
+};
+
+printVechicle(oldCivic);
+
+*/
