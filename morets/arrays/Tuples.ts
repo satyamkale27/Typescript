3ba8d4582740b2439915d893a8a694936f147a81
satyam <satyamkale271@gmail.com>
2025-03-06 18:43:55 +0530

    notes

--- morets/arrays/Tuples.ts
+++ morets/arrays/Tuples.ts
@@ -1,1 +1,14 @@
-const pepsi = ["brown", true, 40];
+const pepsi: [string, boolean, number] = ["brown", true, 40]; // it is a tuple // it has very consistance order of element
+
+pepsi[0] = 10; // gets error by ts
+
+// can also do
+
+/*
+
+type Drink = [string, boolean, number];   // it is Type alias
+
+const pepsi: Drink = ['brown', true, 40];  // can reuse drink
+const sprite: Drink = ['clear', true, 40]; // can reuse drink
+
+*/
